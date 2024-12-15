d64a21e3dcc6a92c1f688a18ce05cff5c6b181b6
danielortiz1982 <danielortiz1982@gmail.com>
2024-12-15 10:28:15 -0500

    First commit

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 3500;
+const PORT = 80;
 const message = `Server is running on port ${PORT}`;
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html");
-});
+app.get("/", (req, res) => res.sendFile("index.html"));
 
 app.listen(PORT, console.log(message));

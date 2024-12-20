fbc1880656cb8f2b631fc6feac6e9f9553c22568
danielortiz1982 <danielortiz1982@gmail.com>
2024-12-20 09:02:14 -0500

    update to put user

--- public/src/views/Home/Home.js
+++ public/src/views/Home/Home.js
@@ -65,7 +65,7 @@ const Home = () => {
   };
 
   const handleOnchange = (e) => {
-    console.log(responseState._id);
+    console.log(responseState);
     const formId = e.target.getAttribute("data-form");
     const updateFormState = f.formEl.map((el) => {
       const updateValue = { ...el, value: e.target.value };
@@ -83,7 +83,10 @@ const Home = () => {
 
     const response = await fetch("http://209.97.154.37/data/v1/user/new", {
       method: "POST",
-      body: JSON.stringify({ ...user, details: flows[counter].formEl }),
+      body: JSON.stringify({
+        ...user,
+        details: [...responseState.details, ...flows[counter].formEl],
+      }),
       headers: myHeaders,
     });
 
